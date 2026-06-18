2581df20cd7a1d54622f5bcb46bc582c95a73b06
elmyrockers <elmyrockers@gmail.com>
2026-06-18 21:40:34 +0800

    Set view engine and render index file

--- client/main.go
+++ client/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+    "github.com/gofiber/fiber/v3"
+    "github.com/gofiber/template/html/v3"
+)
 
 func main() {
-    app := fiber.New()
+    // Use `html/template` view engine
+        engine := html.New("./views", ".html")
+        app := fiber.New(fiber.Config{
+            Views: engine,
+        })
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString( "this is websocket client" )
-    })
+    // Render view file
+        app.Get("/", func(c fiber.Ctx) error {
+            return c.Render("index", fiber.Map{
+                "Username": "elmyrockers",
+            })
+        })
 
     app.Listen(":3000")
 }
