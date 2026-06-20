3bf2c5723a525a4be0b3543eebf5c4a8dc8edb8c
elmyrockers <elmyrockers@gmail.com>
2026-06-20 14:18:25 +0800

    Include middleware's session

--- client/main.go
+++ client/main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/template/html/v3"
 	"github.com/gofiber/fiber/v3/middleware/static"
+	"github.com/gofiber/fiber/v3/middleware/session"
+
+	// "fmt"
 )
 
 func main() {
@@ -13,21 +16,40 @@ func main() {
 			Views: engine,
 		})
 
+	// Set session
+		app.Use(session.New())
+
 	// Route for static files
 		app.Use("/", static.New("./public"))
 
 	// Routes for view
-		app.Get("/", func(c fiber.Ctx) error {
-			return c.Render("index", fiber.Map{
-				"Username": "elmyrockers",
+		// GET
+			app.Get("/", func(c fiber.Ctx) error {
+				return c.Render("index", fiber.Map{})
 			})
-		})
-		app.Get("/chatroom", func(c fiber.Ctx) error {
-			return c.Render("chatroom", fiber.Map{})
-		})
-		app.Post("/", func(c fiber.Ctx) error {
-			return c.Redirect().To("/chatroom")
-		})
+			app.Get("/chatroom", func(c fiber.Ctx) error {
+				sess := session.FromContext(c)
+
+				// Get username
+					username := sess.Get( "username" )
+					if username == nil {
+						return c.Redirect().To("/")
+					}
+					
+				// Render chatroom page
+					return c.Render("chatroom", fiber.Map{
+						"Username": username,
+					})
+			})
+
+		// POST
+			app.Post("/", func(c fiber.Ctx) error {
+				username := c.FormValue( "username" )
+				sess := session.FromContext(c)
+				sess.Set( "username", username )
+
+				return c.Redirect().To("/chatroom")
+			})
 
 	app.Listen(":3000")
 }
